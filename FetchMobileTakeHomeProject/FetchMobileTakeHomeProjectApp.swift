f3bbed6a86c09e9181911e15b7fc6f5b2294fa3c
Crossley Rozario <crozariodev@gmail.com>
2024-11-11 13:50:00 -0500

    Initial Commit

---
+++ FetchMobileTakeHomeProject/FetchMobileTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMobileTakeHomeProjectApp.swift
+//  FetchMobileTakeHomeProject
+//
+//  Created by Crossley Rozario on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchMobileTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
